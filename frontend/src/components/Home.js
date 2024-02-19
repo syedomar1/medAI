7d118f19e9ea7110dd07ef53415ef0e624d23922
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-02-19 21:20:28 +0530

    homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,53 @@
-export const Home = (props) => {
-    const {showAlert} = props
-    return (
-      <div>      
-        <h1>medAI</h1>
+import React from 'react';
+// import './Home.css'
+
+const Home = (props) => {
+  const { showAlert } = props;
+
+  return (
+    <>
+      <div>
+        <img className="vector-icon" src="./public/homebg.png" alt="Vector"/>
+        <div className="or-login-if-container">
+          <span>or </span>
+          <span className="login">Login</span>
+          <span> if you have an account</span>
+        </div>
+        <b className="med-ai">MED AI</b>
+        <div className="empowering-communities-provid">
+          Empowering Communities: Providing Vital Healthcare Services to Those in
+          Need
+        </div>
+        <div className="medai-is-dedicated">
+          MedAI is dedicated to providing free medical services to underprivileged
+          communities in remote areas. Leveraging technology, we deliver
+          healthcare solutions directly to households via smartphones. Our
+          platform offers services like a hospital finder, medical report
+          analysis, and community forums. We aim to raise awareness about health
+          issues and empower individuals to take control of their well-being. With
+          our AI bot, users can receive personalized healthcare insights. Join us
+          in democratizing healthcare access for all.
+        </div>
+        <img
+          className="unnamed-removebg-preview-1-icon"
+          alt="Preview Image 1"
+          src="/unnamedremovebgpreview-1@2x.png"
+        />
+        <img
+          className="pexels-karolina-grabowska-4386-icon"
+          alt="Preview Image 2"
+          src="/pexelskarolinagrabowska4386464-1@2x.png"
+        />
+        <div className="frame-wrapper">
+          <div className="sign-up-now-wrapper">
+            <div className="sign-up-now">Sign Up now</div>
+          </div>
+        </div>
+        <b className="about-us1">ABOUT US</b>
+        <img className="frame-child" alt="Group 2" src="/group-2.svg" />
       </div>
-    );
-  };
-  
-  export default Home;
+    </>
+  );
+};
+
+export default Home;
