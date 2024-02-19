09557461e777adbc2d2c0b2f484186310de55f6c
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-02-19 16:09:08 +0530

    Added initial folders

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import ContextState from "./context/notes/ContextState";
+import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import {useState} from 'react'
+
+
+function App() {
+  const [alert,setAlert] = useState(null);
+  const showAlert = (message,type) =>{
+    setAlert({
+      msg:message,
+      type:type});
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  return (
+    <>
+      <ContextState>
+        <Router>
+          <Navbar title="MedAI" aboutText="About" />
+          <Alert alert={alert}/>
+          <div className="container" style={{backgroundColor: "lightblue",minHeight:"50vh"}}>
+            <Routes>
+              <Route exact path="/" element={<Home showAlert={showAlert}/>} />
+              <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login showAlert={showAlert}/>} />
+              <Route exact path="/signup" element={<Signup showAlert={showAlert}/>} />
+            </Routes>
+          </div>
+        </Router>
+      </ContextState>
+    </>
+  );
+}
+
+export default App;
