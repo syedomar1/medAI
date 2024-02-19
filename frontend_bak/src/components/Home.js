44a331f8e8cdec1fc2e02a7c5f59e2b47799c4dd
Amritanshu Tripathy <tripathyamritanshu7@gmail.com>
2024-02-19 22:18:35 +0530

    Added frontend_bak

---
+++ frontend_bak/src/components/Home.js
@@ -0,0 +1,10 @@
+export const Home = (props) => {
+    const {showAlert} = props
+    return (
+      <div>      
+        <h1>medAI</h1>
+      </div>
+    );
+  };
+  
+  export default Home;
---
+++ frontend_bak/src/components/Signup.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom';
+
+const Signup = (props) => {
+    return (
+        <div>      
+        </div>
+      );
+}
+export default Signup
